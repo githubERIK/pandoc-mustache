0d4a54f736ac763d8c54382c83d8e2246bf18264
githubERIK <githubERIK@users.noreply.github.com>
2020-01-25 14:11:33 +0200

    Drop older python versions support

--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ setup(
     long_description=long_description,
     # The project's main homepage.
     url="https://github.com/michaelstepner/pandoc-mustache/",
-    # Author details
-    author="Michael Stepner",
-    author_email="stepner@mit.edu",
+    # Original Author details
+    # author="Michael Stepner",
+    # author_email="stepner@mit.edu",
     # Choose your license
     license="CC0-1.0",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -60,58 +60,16 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 3.8",
     ],
     # What does your project relate to?
     keywords="pandoc pandocfilters panflute markdown latex mustache",
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=["contrib", "docs", "tests", "examples"]),
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    # py_modules=["pandoc_mustache"],
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        "panflute==1.12.4",
-        "pystache==0.5.4",
-        "pyyaml==5.2",
-        "future==0.16.0",
-    ],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        #     'dev': ['check-manifest'],
-        #     'test': ['pandocfilters', 'configparser', 'pytest-cov', 'future'],
-        "pypi": ["docutils"]
-    },
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #    'sample': ['package_data.dat'],
-    # },
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
+    install_requires=["panflute==1.12.4", "pystache==0.5.4", "pyyaml==5.3",],
     entry_points={
         "console_scripts": ["pandoc-mustache=pandoc_mustache.pandoc_mustache:main",],
     },
+    python_requires="~=3.6",
 )
